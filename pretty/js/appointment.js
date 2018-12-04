ee77fe0813eec90f6c16d95c6900029d3df5bb55
danieljanderson <danieljanderson0616@gmail.com>
2018-12-03 19:17:55 -0500

    the appointment calender works but it is not allowing me to us the functions

--- pretty/js/appointment.js
+++ pretty/js/appointment.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
+  var calender= document.querySelectorAll('.datepicker');
+  console.log(calender)
+  
+  var instances = M.Datepicker.init(calender, {format:'mmmm dd, yyyy'});
+  console.log(instances.close())
+  //var element = document.querySelectorAll('.datepicker');
+  //instance.SetDate()
+  //var test = M.Datepicker.getInstance(calender)
+  //console.log(instances.SetDate())
+  //instance.setDate()
+});
+
+
+
 
