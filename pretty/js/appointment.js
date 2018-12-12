4a7e1c806af990bf47bf385be4c11b6b677d1717
danieljanderson <danieljanderson0616@gmail.com>
2018-12-11 20:19:42 -0500

    variable saved in on select

--- pretty/js/appointment.js
+++ pretty/js/appointment.js
@@ -1,26 +1,18 @@
+var appointmentDate
 document.addEventListener('DOMContentLoaded', function() {
-  //var calender= document.querySelectorAll('.datepicker');
- // console.log(calender)
- //var date = M.Datepicker.getInstance('.datepicker').toString(); 
-  
-  //var instances = M.Datepicker.init(calender, {format:'mmmm dd, yyyy'});
-  //var date =M.Datepicker.getInstance('.datepicker').toString();
-  //console.log(date)
-  //var appointmentDate = M.Datepicker.date
- // console.log("this should be the appointmentDate ",instances)
-  //var element = document.querySelectorAll('.datepicker');
-  //instance.SetDate()
-  //var test = M.Datepicker.getInstance(calender)
-  //console.log(instances.SetDate())
-  //instance.setDate()
-  var elems = document.querySelectorAll('.datepicker');
-  console.log(elems)
-  M.Datepicker.init(elems);
-  
-  
-  var instance = M.Datepicker.getInstance(elems[0]);
-  console.log(instance.date)
-  
+    
+    var options = {
+        format: 'mmmm dd, yyyy',
+        onSelect: function(date) { 
+             appointmentDate= date
+             console.log(appointmentDate)
+            
+        }
+    }
+
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+    
    
 });
 
