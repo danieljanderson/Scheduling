d37f87c9960cc4d97f04fbcfc44fdfe6fc973d39
danieljanderson <danieljanderson0616@gmail.com>
2018-12-12 20:02:05 -0500

    right format for date

--- pretty/js/appointment.js
+++ pretty/js/appointment.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var options = {
         format: 'mmmm dd, yyyy',
         onSelect: function(date) { 
+             //the appointmentDate is the date the user chooses for their appointment
              appointmentDate= date
              console.log(appointmentDate)
             
