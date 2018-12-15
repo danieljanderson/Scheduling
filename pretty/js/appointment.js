79330382b8c4976a14f1b55cc6ea042e77addb9d
danieljanderson <danieljanderson0616@gmail.com>
2018-12-15 13:24:48 -0500

    got the seconds and hours to display in a variable mode

--- pretty/js/appointment.js
+++ pretty/js/appointment.js
@@ -1,4 +1,5 @@
 var appointmentDate
+var appointmentTime
 document.addEventListener('DOMContentLoaded', function() {
     
     var options = {
@@ -7,12 +8,32 @@ document.addEventListener('DOMContentLoaded', function() {
              //the appointmentDate is the date the user chooses for their appointment
              appointmentDate= date
              console.log(appointmentDate)
+              //getAppointmentTime()
             
         }
     }
 
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, options);
+})
+document.addEventListener('DOMContentLoaded',function(){
+    //returns the selected time the user picks
+        var timeOption = {
+            twelveHour:true,
+            vibrate:true,
+            onSelect: function(hour,second){
+                var hourtime = hour
+                var secondtime = second
+                console.log(hourtime,"this is the time on the select is the hour"+"and"+secondtime+"is the second time")
+                }
+            }
+            
+        
+        var timeElems = document.querySelectorAll('.timepicker');
+        var timeInstances = M.Timepicker.init(timeElems, timeOption,time);
+        var time= timeInstances['time']
+        console.log("this is the time"+time)
+    
     
    
 });
