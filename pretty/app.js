d0ee1f6a06044c30b6c34a9b333cc1b02582cd83
danieljanderson <danieljanderson0616@gmail.com>
2019-07-30 15:49:30 -0400

    setting up express

---
+++ pretty/app.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express()
+
+// var port = 3000
+// var mongoose = require('mongoose')
+// var mongoDB = 'mongodb://127.0.0.1/my_database'
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
+// var nameSchema = new mongoose.Schema({
+//   firstName: String,
+//   lastNameName: String,
+//   emailAddress: String,
+//   phoneNumber: String,
+//   appointmentDate: String,
+//   appointmentTime: String,
+// })
+
+// app.listen(port, () => {
+//   console.log('Server listening on port ' + port)
+// })
