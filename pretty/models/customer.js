186b77a1b99de37c14b1ea3cfe9379ae78dd09e8
danieljanderson <danieljanderson0616@gmail.com>
2019-10-10 11:28:53 -0400

    added validation to email

--- pretty/models/customer.js
+++ pretty/models/customer.js
@@ -4,10 +4,18 @@ const Schema = mongoose.Schema
 const CustomerSchema = new Schema({
   firstName: {
     type: String,
+    validate: {
+      validator: firstName => firstName.length > 0,
+      message: 'first name must be longer than 0 characters',
+    },
     required: [true, 'First Name is required'],
   },
   lastName: {
     type: String,
+    validate: {
+      validator: lastName => lastName.length > 0,
+      message: 'last name must be longer than 0 characters',
+    },
     required: [true, 'Last Name is required'],
   },
   phoneNumber: {
@@ -15,6 +23,13 @@ const CustomerSchema = new Schema({
   },
   email: {
     type: String,
+    validate: {
+      validator: email => {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return re.test(email)
+      },
+      message: 'that is not a valid email',
+    },
     required: [true, 'Email is required'],
   },
 })
--- pretty/test/validation_test.js
+++ pretty/test/validation_test.js
@@ -19,4 +19,28 @@ describe('Validating records', () => {
         emailMessage === 'Email is required'
     )
   })
+  it('requires customers  and a valid email.', () => {
+    const customer = new Customer({
+      firstName: '',
+      lastName: '',
+      email: 'a',
+    })
+    const validationResult = customer.validateSync()
+    console.log(validationResult)
+    const firstNameMessage = validationResult.errors.firstName.message
+    const lastNameMessage = validationResult.errors.lastName.message
+    const emailMessage = validationResult.errors.email.message
+    console.log('this is firstname message')
+    console.log(firstNameMessage)
+    console.log('this is lastname message')
+    console.log(lastNameMessage)
+    console.log('this is email message')
+    console.log(emailMessage)
+
+    assert(
+      //firstNameMessage === 'first name must be longer than 0 characters' &&
+      //lastNameMessage === 'last name must be longer than 0 characters' &&
+      emailMessage === 'that is not a valid email'
+    )
+  })
 })
