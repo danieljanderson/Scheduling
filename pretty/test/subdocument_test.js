777893df97a27adbb64769fc6be1fcefb2f0976f
danieljanderson <danieljanderson0616@gmail.com>
2019-10-20 21:25:59 -0400

    can save a record to collection

--- pretty/test/subdocument_test.js
+++ pretty/test/subdocument_test.js
@@ -1,13 +1,22 @@
 const assert = require('assert')
 const Customer = require('../models/customer')
-
-describe('Subdocuments', () => {
-  it('can create a subdocument', done => {
+const testDate = new Date(2018, 11, 24, 10, 33, 30, 0)
+describe('saving', () => {
+  it('it can save a record', done => {
     const joe = new Customer({
       firstName: 'Joe',
       lastName: 'Smith',
       email: 'na@na.com',
       appointments: [{ AppointmentDate: new Date(2018, 11, 24, 10, 33, 30, 0) }],
     })
+    joe
+      .save()
+      .then(() => Customer.findOne({ firstName: 'Joe' }))
+      .then(customer => {
+        console.log('this is the test date', testDate)
+        console.log('this is the appointments date', customer.appointments[0].AppointmentDate)
+        assert(customer.appointments[0].AppointmentDate.getTime() === testDate.getTime())
+        done()
+      })
   })
 })
--- pretty/test/test_helper.js
+++ pretty/test/test_helper.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 before(done => {
   mongoose.connect('mongodb://localhost/scheduling_test', { useNewUrlParser: true })
   mongoose.connection
-    .once('open', () => done())
+    .once('open', () => console.log('good to go'))
     .on('error', err => {
       console.warn('Warning', error)
     })
+    .then(() => done())
 })
 beforeEach(done => {
   const { customers } = mongoose.connection.collections
