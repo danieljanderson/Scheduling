692490151d77e9146f5428afb705d63169ac03bf
danieljanderson <danieljanderson0616@gmail.com>
2019-11-24 17:58:59 -0500

    added port variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const app = require('./app')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = require('./app');
+const PORT = process.env.PORT || 3050;
 
-app.listen(3050, () => {
-  console.log('Running on port 3050')
-})
+app.listen(PORT, () => {
+  console.log('Running on port ' + PORT);
+});
